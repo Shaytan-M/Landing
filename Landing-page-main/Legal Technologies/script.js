a6e1eed183a42ba1280619b1fe5aaed7fffbc6f3
Shaytan-M <73448452+Shaytan-M@users.noreply.github.com>
2022-06-08 02:46:50 +0300

    Landing

---
+++ Landing-page-main/Legal Technologies/script.js
@@ -0,0 +1,11 @@
+let burgerMenu = document.querySelector('.burgerMenu')
+let sideBar = document.querySelector('.blockSideBar')
+
+
+function openBurger(){
+    burgerMenu.children[0].classList.toggle('menu-top-click')
+    burgerMenu.children[1].classList.toggle('menu-middle-click')
+    burgerMenu.children[2].classList.toggle('menu-bottom-click')
+    sideBar.classList.toggle('activeSideBar')
+    console.log(sideBar);
+} 
